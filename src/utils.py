4bff1244c54a940dd4e03db95994a7770e6a896f
zhaipro <zhaijiaoshou@gmail.com>
2019-05-05 18:37:57 +0800

    Update downsampling

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from cv2 import imread, imwrite, resize
+import cv2
 import numpy as np
 
 
@@ -21,6 +22,11 @@ def flip(im):
 
 def downsampling(im, scale):
     size = im.shape[1] // scale, im.shape[0] // scale
+    hidden_scale = np.random.uniform(1, 3)
+    hidden_size = int(size[0] / hidden_scale), int(size[1] / hidden_scale)
+    radius = np.random.uniform(1, 3)
+    im = cv2.GaussianBlur(im, (5, 5), radius)
+    im = resize(im, hidden_size)
     return resize(im, size)
 
 
